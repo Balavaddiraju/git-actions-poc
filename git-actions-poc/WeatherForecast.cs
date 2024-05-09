700695c1b51a91ee561e3efad1a2a76a81cb2a93
Balu <balumca2004@gmail.com>
2024-05-09 15:13:27 +0530

    added datetime property..

--- git-actions-poc/WeatherForecast.cs
+++ git-actions-poc/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace git_actions_poc
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
