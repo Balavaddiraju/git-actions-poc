690772e1742245a037cce9bf843c09b59b34308b
Balu <balumca2004@gmail.com>
2024-05-13 18:04:18 +0530

    changed to full date

--- git-actions-poc/WeatherForecast.cs
+++ git-actions-poc/WeatherForecast.cs
@@ -9,6 +9,6 @@ namespace git_actions_poc
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        public DateTime FullDateTime { get; set; }
+        public DateTime CompleteDateTime { get; set; }
     }
 }
